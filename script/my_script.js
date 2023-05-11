62205656a8affcf8e5c2630a1e41baf4e60821c9
EmanuelaPau <uapame@gmail.com>
2023-05-11 14:55:50 +0200

    Change lets in const

--- script/my_script.js
+++ script/my_script.js
@@ -6,19 +6,19 @@ console.log('hello');
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 // Chiedi all’utente il suo nome,
-let myName = prompt("Qual è il tuo nome?");
+const myName = prompt("Qual è il tuo nome?");
 console.log(myName);
 
 // poi chiedi il suo cognome,
-let surname = prompt("Qual è il tuo cognome?");
+const surname = prompt("Qual è il tuo cognome?");
 console.log(surname);
 
 // poi chiedi il suo colore preferito
-let favNumber = prompt("Qual è il tuo numero preferito?");
+const favNumber = prompt("Qual è il tuo numero preferito?");
 console.log(favNumber);
 
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
-let yourPassword = myName + surname + favNumber + '21';
+const yourPassword = myName + surname + favNumber + '21';
 console.log(yourPassword);
 
 document.getElementById("my_magicpassword").textContent = "Your Magic Super Password is: " + yourPassword;;
