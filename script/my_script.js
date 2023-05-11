9b261f1be709ed9de3a37f71aea9df27eaa6f173
EmanuelaPau <uapame@gmail.com>
2023-05-11 14:27:55 +0200

    Add name var

--- script/my_script.js
+++ script/my_script.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+// Chiedi all’utente il suo nome,
+let name = prompt("Come ti chiami?");
+console.log(name);
