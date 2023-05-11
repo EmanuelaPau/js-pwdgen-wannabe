df006ac660d9b23d3b9af6e511cd89263c3533aa
EmanuelaPau <uapame@gmail.com>
2023-05-11 14:32:20 +0200

    Add var password

--- script/my_script.js
+++ script/my_script.js
@@ -6,9 +6,17 @@ console.log('hello');
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 // Chiedi all’utente il suo nome,
-let name = prompt("Qual è il tuo nome?");
-console.log(name);
+let myName = prompt("Qual è il tuo nome?");
+console.log(myName);
 
 // poi chiedi il suo cognome,
 let surname = prompt("Qual è il tuo cognome?");
-console.log(surname);   
+console.log(surname);
+
+// poi chiedi il suo colore preferito
+let favNumber = prompt("Qual è il tuo numero preferito?");
+console.log(favNumber);
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+let yourPassword = myName + surname + favNumber + '21';
+console.log(yourPassword);
